5f6ca678167bf0439b36a6395ade5dc60d4224ea
marcielle <marcielle0644@gmail.comm>
2023-06-26 15:19:22 -0300

    wip

--- app/Http/Controllers/DownloadCurriculoController.php
+++ app/Http/Controllers/DownloadCurriculoController.php
@@ -5,43 +5,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
-use Illuminate\Support\Str;
 use Spatie\Browsershot\Browsershot;
-use Spatie\TemporaryDirectory\TemporaryDirectory;
+use Illuminate\Support\Str;
+use Symfony\Component\HttpFoundation\Response;
 
 class DownloadCurriculoController extends Controller
 {
-
-
-
-
-    // public function submitForm(Request $request)
-    // {
-    //     $css = file_get_contents(public_path('css/curriculo.css'));
-    //     $html = $request->input('cv'); // Retrieve the HTML value from the request
-    //     $html = '<style>' . $css . '</style>' . $html;
-
-    //     try {
-    //         $pdfFilePath = public_path('css/example.pdf');
-
-
-    //         Browsershot::html($html)
-    //             ->noSandbox()
-    //             ->save($pdfFilePath);
-
-    //         $pdf = file_get_contents($pdfFilePath);
-
-    //         $headers = [
-    //             'Content-Type' => 'application/pdf',
-    //             'Content-Disposition' => 'attachment; filename="example.pdf"',
-    //         ];
-
-    //         return response($pdf, 200, $headers);
-    //     } catch (\Exception $e) {
-    //         // Log or print the exception message for debugging
-    //         dd($e->getMessage());
-    //     }
-    // }
 
     public function submitForm(Request $request)
     {
@@ -50,26 +19,24 @@ class DownloadCurriculoController extends Controller
         $html = '<style>' . $css . '</style>' . $html;
 
         try {
-            // $pdfFilePath = sys_get_temp_dir() . '/' . Str::random(16) . '.pdf';
-            $pdfFilePath = public_path('css/CurrÃ­culo.pdf');
+            $pdfFilePath = 'pdf/' . Str::random(16) . '.pdf'; // Path to store the PDF file in the storage directory
+
             Browsershot::html($html)
                 ->noSandbox()
-                ->save($pdfFilePath);
+                ->save(storage_path('app/pdf/' . $pdfFilePath));
 
-            $pdf = file_get_contents(public_path('css/CurrÃ­culo.pdf'));
+
+            $pdfContents = Storage::disk('local')->download('app/pdf/' . $pdfFilePath, $pdfFilePath);
+            // Delete the PDF file from storage
+            // Storage::disk('local')->delete($pdfFilePath);
 
             $headers = [
                 'Content-Type' => 'application/pdf',
-                'Content-Disposition' => 'attachment; filename="CurrÃ­culo.pdf"',
+                'Content-Disposition' => 'attachment; filename="Curriculo.pdf"',
             ];
 
-            // Delete the temporary PDF file
-
-
-            return response()->download($pdfFilePath, 'example.pdf', $headers)
-                ->deleteFileAfterSend(true);
+            return response($pdfContents, 200, $headers);
         } catch (\Exception $e) {
-            // Log or print the exception message for debugging
             dd($e->getMessage());
         }
     }
